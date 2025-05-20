07eeae86553a462e878de38f8e07af7bf3148a8a
efegurkann <efegurkans@outlook.com>
2025-05-20 11:40:08 +0300

    DatabaseSeeder.php dosyasına QuestionSeeder sınıfı eklendi.

---
+++ app/Models/Question.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['quiz_id', 'question', 'image', 'answer1', 'answer2', 'answer3', 'answer4', 'correct_answer'];
+
+    //
+}
