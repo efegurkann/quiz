205608b6aaa1da3f0a3192bfc0c49925d9e789bf
efegurkann <efegurkans@outlook.com>
2025-05-28 19:34:54 +0300

    MainController'a quiz sonuçlarını işleyen yeni bir result fonksiyonu eklendi. Kullanıcı cevapları kaydedilerek, doğru ve yanlış cevap sayıları hesaplandı. Ayrıca, sonuçlar Result modeline kaydedildi. DatabaseSeeder'a ResultSeeder eklendi.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Quiz;
 use Illuminate\Http\Request;
+use App\Models\Answer;
+use App\Models\Result;
 
 class MainController extends Controller
 {
@@ -30,8 +32,35 @@ class MainController extends Controller
 
     public function result(Request $request, $slug)
     {
-        return $request->post();
+        $quiz = Quiz::with('questions')->whereSlug($slug)->first() ?? abort(404, 'Quiz Bulunamadı');
+        $correct = 0;
+
+        foreach ($quiz->questions as $question) {
+
+            Answer::create([
+                'user_id' => auth()->user()->id,
+                'question_id' => $question->id,
+                'answer' => $request->post($question->id),
+            ]);
+
+            echo $question->correct_answer . ' - ' . $request->post($question->id) . '<br>';
+
+            if ($question->correct_answer === $request->post($question->id)) {
+                $correct+=1;
+            }
+        }
+
+        $point = round(100 / count($quiz->questions)) * $correct;
+        $wrong = count($quiz->questions) - $correct;
+
+        Result::create([
+            'user_id' => auth()->user()->id,
+            'quiz_id' => $quiz->id,
+            'point' => $point,
+            'correct' => $correct,
+            'wrong' => $wrong
+        ]);
+
+        return redirect()->route('quiz.detail', $quiz->slug)->with('success', 'Quiz başarıyla tamamlandı. Puanınız: ' . $point);
     }
 }
-
-
--- app/Models/Result.php
+++ app/Models/Result.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-class Answer extends Model
+
+class Result extends Model
 {
     use HasFactory;
 
-    public $timestamps = false;
-
-
-    protected $fillable = ['user_id', 'question_id', 'answer'];
+    protected $fillable = ['user_id', 'quiz_id', 'point', 'correct', 'wrong'];
 }
